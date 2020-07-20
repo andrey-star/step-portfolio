5956279d9c8d64b23b9495abff54ae980d039787
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-07-20 16:39:21 +0300

    Add hardcoded JSON response.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,6 +1,10 @@
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,9 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private final List<String> COMMENTS = Arrays.asList("test", "json", "response");
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Andrey!</h1>");
+    response.setContentType("application/json;");
+    response.getWriter().println(convertToJsonUsingGson(COMMENTS));
+  }
+
+  private String convertToJsonUsingGson(Object o) {
+    return new Gson().toJson(o);
   }
 }
