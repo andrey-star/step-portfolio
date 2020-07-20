9faacad7f39ee8b33ad06e8aac1902989d1ae1e9
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-07-20 19:39:32 +0300

    Add in-memory comment section.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -3,8 +3,10 @@ package com.google.sps.servlets;
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,12 +15,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  private final List<String> COMMENTS = Arrays.asList("test", "json", "response");
+  private final List<String> comments = new ArrayList<>();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    System.out.println("GET");
     response.setContentType("application/json;");
-    response.getWriter().println(convertToJsonUsingGson(COMMENTS));
+    response.getWriter().println(convertToJsonUsingGson(comments));
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String comment = getParameter(request, "user-comment", "");
+    if (!comment.isEmpty()) {
+      comments.add(comment);
+    }
+    response.sendRedirect("/index.html");
+  }
+
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
   }
 
   private String convertToJsonUsingGson(Object o) {
