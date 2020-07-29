636604c8db1fbe7be46fd6b145aa84fa7d6980d8
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-07-29 20:31:51 +0300

    Add scheduling algorithm.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,23 +1,72 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class FindMeetingQuery {
+
+  private static final int MINUTES_IN_DAY = 60 * 24;
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // Generate a list of meeting actions (start or end) of the required attendees sorted by time.
+    // The order of 'start' and 'end' events with equal time permits 0 minute long meetings.
+    List<MeetingAction> sortedAttendeeEvents = events.stream()
+            .filter(event -> event.getAttendees().stream().anyMatch(request.getAttendees()::contains))
+            .map(Event::getWhen)
+            .flatMap(event -> Stream.of(
+                    new MeetingAction(event.start(), MeetingActionType.START),
+                    new MeetingAction(event.end(), MeetingActionType.END)))
+            .sorted(Comparator.comparing(MeetingAction::getTime).thenComparing(MeetingAction::getType))
+            .collect(Collectors.toList());
+
+    int curBusy = 0;
+    int curFreeStart = 0;
+    List<TimeRange> timeRanges = new ArrayList<>();
+    for (MeetingAction event : sortedAttendeeEvents) {
+      if (event.getType() == MeetingActionType.START) {
+        TimeRange timeRange = new TimeRange(curFreeStart, event.getTime() - curFreeStart);
+        if (curBusy == 0 && timeRange.duration() >= request.getDuration()) {
+          timeRanges.add(timeRange);
+        }
+        curBusy++;
+      } else {
+        curBusy--;
+        if (curBusy == 0) {
+          curFreeStart = event.getTime();
+        }
+      }
+    }
+    TimeRange timeRangeUntilEndOfDay = new TimeRange(curFreeStart, MINUTES_IN_DAY - curFreeStart);
+    if (timeRangeUntilEndOfDay.duration() >= request.getDuration()) {
+      timeRanges.add(timeRangeUntilEndOfDay);
+    }
+    return timeRanges;
+  }
+
+  private static class MeetingAction {
+    private final int time;
+    private final MeetingActionType type;
+
+    public MeetingAction(int time, MeetingActionType type) {
+      this.time = time;
+      this.type = type;
+    }
+
+    public int getTime() {
+      return time;
+    }
+
+    public MeetingActionType getType() {
+      return type;
+    }
+  }
+
+  private enum MeetingActionType {
+    END,
+    START
   }
 }
